b798a9da2f598e00a351c70ef32824e270b86893
SeuNome <seuemail@gmail.com>
2026-09-04 12:27:46 -0300

    Atualizar configuracao do Flask

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-from flask import Flask, request
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def inicio():
-    print("IP:", request.remote_addr)
-    print("Navegador:", request.headers.get("User-Agent"))
-    print("Idioma:", request.headers.get("Accept-Language"))
-
-    return "Dados registrados no Termux!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000)
-
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 10000))
+    )
